977e890ad43d5a45fbbd8d408ea7f326dbc73b91
mpratama17 <yogmhmmd17@gmail.com>
2023-03-30 12:41:55 +0700

    create custom button sign in and sign up

--- lib/custom_widgets/customwidgets.dart
+++ lib/custom_widgets/customwidgets.dart
@@ -17,3 +17,25 @@ TextField reusableTextfield(String text, IconData icon, bool isPasswordType,
         isPasswordType ? TextInputType.visiblePassword : TextInputType.text,
   );
 }
+
+Container SignInSignUpButton(
+    BuildContext context, bool isLogin, Function onTap) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    height: 50,
+    margin: EdgeInsets.fromLTRB(0, 10, 0, 20),
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+    child: ElevatedButton(
+      onPressed: () {
+        onTap();
+      },
+      child: Text(
+        isLogin ? 'Sign In' : 'Sign Up',
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+  );
+}
