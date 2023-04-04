fbfc61b5d9013b93ec2cd0cd47378dbd7d2625d5
mpratama17 <yogmhmmd17@gmail.com>
2023-04-04 13:07:17 +0700

    setup route to firebase

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:tes/custom_widgets/customwidgets.dart';
+import 'package:tes/view/home.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -16,16 +18,46 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            reusableTextfield("email", Icons.abc, false, _emailController),
-            SizedBox(height: 16),
-            reusableTextfield("password", Icons.lock, true, _passwordController)
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 400,
+                ),
+                reusableTextfield("email", Icons.abc, false, _emailController),
+                SizedBox(height: 16),
+                reusableTextfield(
+                    "password", Icons.lock, true, _passwordController),
+                SizedBox(height: 16),
+                SignInSignUpButton(context, true, () {
+                  FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                          email: _emailController.text,
+                          password: _passwordController.text)
+                      .then((value) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home()),
+                    );
+                  }).onError((error, stackTrace) {
+                    SnackBar(content: Text("Password or Email is wrong"));
+                  });
+                })
+              ],
+            ),
+          ),
         ),
       ),
     );
+    //create function for signin firebase
   }
+
+  // Future signIn() async {
+  //   await FirebaseAuth.instance.signInWithEmailAndPassword(
+  //       email: _emailController.text.trim(),
+  //       password: _passwordController.text.trim());
+  // }
 }
