0eecb166ffa702e5c41d69138fc1525248b90328
mpratama17 <yogmhmmd17@gmail.com>
2023-04-04 13:07:10 +0700

    setup route to firebase

--- lib/view/signup.dart
+++ lib/view/signup.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:tes/view/home.dart';
+
+import '../custom_widgets/customwidgets.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -14,6 +18,44 @@ class _SignUpScreenState extends State<SignUpScreen> {
   TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      extendBodyBehindAppBar: false,
+      appBar: AppBar(
+        // backgroundColor: Colors.blue,
+        elevation: 0,
+        title: const Text(
+          "Sign Up",
+          style: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              reusableTextfield("email", Icons.abc, false, _emailController),
+              SizedBox(height: 16),
+              reusableTextfield(
+                  "password", Icons.lock, true, _passwordController),
+              SizedBox(height: 16),
+              SignInSignUpButton(context, false, () {
+                FirebaseAuth.instance
+                    .createUserWithEmailAndPassword(
+                        email: _emailController.text,
+                        password: _passwordController.text)
+                    .then((value) {
+                  SnackBar(content: Text("Sign Up Success"));
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => Home()));
+                }).onError((error, stackTrace) {
+                  print("error");
+                });
+              })
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
