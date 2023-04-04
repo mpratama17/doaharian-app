37eef42716a019adb6cc6c21c7faf75fa3e1b907
mpratama17 <yogmhmmd17@gmail.com>
2023-04-04 13:09:15 +0700

    no change

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:tes/custom_widgets/navigation_bar.dart';
+import 'package:tes/view/detail_baru.dart';
 import 'package:tes/view/login.dart';
+import 'package:tes/view/signup.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
 import 'view/home.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
+// void main() {
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,9 +26,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Doa Harian',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
+        fontFamily: GoogleFonts.poppins().fontFamily,
       ),
       home: LoginScreen(),
     );
