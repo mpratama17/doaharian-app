71f2f7d0549f80110dd7cf21b2b3af9699981965
mpratama17 <yogmhmmd17@gmail.com>
2023-05-08 19:20:48 +0700

    add screen location in navbar

--- lib/custom_widgets/navigation_bar.dart
+++ lib/custom_widgets/navigation_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 import '../view/home.dart';
+import '../view/location.dart';
 import '../view/user.dart';
 
 class NavBar extends StatefulWidget {
@@ -14,10 +15,7 @@ class NavBar extends StatefulWidget {
 class _NavBarState extends State<NavBar> {
   int _currentIndex = 0;
   //create list for bottom nav bar
-  List screens = [
-    Home(),
-    UserProfile(),
-  ];
+  List screens = [Home(), Loc(), UserProfile()];
   void onTap(int index) {
     setState(() {
       _currentIndex = index;
@@ -27,22 +25,27 @@ class _NavBarState extends State<NavBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: screens[_currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "User",
-            ),
-          ],
-          onTap: onTap,
-          currentIndex: _currentIndex,
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: Colors.blue[900],
-        ));
+      body: screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            label: "Location",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "User",
+          ),
+        ],
+        onTap: onTap,
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blue[900],
+      ),
+    );
   }
 }
