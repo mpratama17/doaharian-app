4ed3aa5dbe231e1c32f2c6905948397f19212803
mpratama17 <yogmhmmd17@gmail.com>
2023-05-08 19:19:35 +0700

    for theme

---
+++ lib/theme.dart
@@ -0,0 +1,15 @@
+//create themedata
+import 'package:flutter/material.dart';
+
+ThemeData light = ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.indigo,
+  // accentColor: Colors.pink,
+  // scaffoldBackgroundColor: Colors.cyan);
+);
+
+ThemeData dark = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.indigo,
+    // accentColor: Colors.pink,
+    scaffoldBackgroundColor: Colors.deepOrangeAccent);
