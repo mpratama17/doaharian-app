4e99047e8ea9c56b4fd023533311ee10bbe6b9d4
mpratama17 <yogmhmmd17@gmail.com>
2023-05-16 07:38:08 +0700

    add dummy info user

--- lib/view/detail.dart
+++ lib/view/detail.dart
@@ -21,17 +21,30 @@ class _DetailState extends State<Detail> {
       appBar: AppBar(
         title: Text(widget.item.doa!),
       ),
-      body: Padding(padding: EdgeInsets.all(16), 
-      child: Column(
-        children: [
-          SizedBox(height: 16),
-          Text(widget.item.ayat!,
-          textAlign: TextAlign.end,
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-          SizedBox(height: 12),
-          Text(widget.item.artinya!, textAlign: TextAlign.justify,),
-        ],
-      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 12, left: 12, right: 12),
+        child: Column(
+          children: [
+            SizedBox(height: 16),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                widget.item.ayat!,
+                textAlign: TextAlign.right,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 12),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                widget.item.artinya!,
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
