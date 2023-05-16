28687227513c26da000c22ab239e0921db5b98f4
mpratama17 <yogmhmmd17@gmail.com>
2023-05-16 07:37:01 +0700

    add code for user keep login

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tes/custom_widgets/navigation_bar.dart';
-import 'package:tes/theme.dart';
-import 'package:tes/view/detail_baru.dart';
-import 'package:tes/view/location.dart';
+import 'package:tes/theme/theme_constant.dart';
+import 'package:tes/view/home.dart';
 import 'package:tes/view/login.dart';
-import 'package:tes/view/signup.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
-import 'view/home.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-// void main() {
-//   runApp(const MyApp());
-// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  User? user;
+
+  void initState() {
+    super.initState();
+    user = FirebaseAuth.instance.currentUser;
+    // print("id user $user?.uid.toString()");
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Doa Harian',
       theme: light,
-      home: Loc(),
+      title: 'Doa Harian',
+      home: user != null ? NavBar() : LoginScreen(),
     );
   }
 }
