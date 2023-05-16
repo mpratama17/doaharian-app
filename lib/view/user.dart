d1c66e6e18f7b615f2664058a25db2a16e49f1f1
mpratama17 <yogmhmmd17@gmail.com>
2023-05-16 07:52:49 +0700

    update ui, data dummy

--- lib/view/user.dart
+++ lib/view/user.dart
@@ -12,17 +12,85 @@ class UserProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child:
-              //create button to logout
-              ElevatedButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut().then((value) {
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) => LoginScreen()));
-              });
-            },
-            child: Text("Logout"),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 70,
+                backgroundImage: AssetImage('assets/profil.jpeg'),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.person,
+                      color: Colors.black,
+                    ),
+                    title: Text('Mohammad Yoga Pratama'),
+                  )),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone_android,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    '08765865xxx',
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.location_on,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'Malang, Indonesia',
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 18,
+                    backgroundImage: AssetImage('assets/github.png'),
+                  ),
+                  title: Text(
+                    'github.com/mpratama17',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: 200,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut().then((value) {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginScreen()));
+                    });
+                  },
+                  child: Text(
+                    "Logout",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
