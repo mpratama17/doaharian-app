f13bc23a93bdd344593c62814160f65318129796
mpratama17 <yogmhmmd17@gmail.com>
2023-05-16 07:53:18 +0700

    update ui

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:tes/repository.dart';
+import 'package:tes/repository_jadwal_sholat.dart';
+import 'package:tes/view/login.dart';
 import 'package:tes/view/user.dart';
+import '../model/model_jadwal_sholat.dart';
+import '../view/location.dart';
 
 import '../model/model.dart';
 import 'detail.dart';
@@ -15,56 +17,88 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<Doa> listdoa = [];
+  // List<Sholat> listSholat = [];
 
   Repository repository = Repository();
+  RepositorySholat repositorySholats = RepositorySholat();
 
   getData() async {
     listdoa = await repository.getData();
     setState(() {});
   }
 
+  // getDataSholat() async {
+  //   listSholat = await repositorySholats.getDataSholats();
+  //   setState(() {});
+  // }
+
   @override
   void initState() {
     getData();
+    // getDataSholat();s
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(title: Text('Doa Harian', 
-      // style: TextStyle(fontWeight: FontWeight.bold),), 
-      // elevation: 0,
-      // actions: [
-      //   IconButton(
-      //     onPressed: () {
-      //       // Navigator.push(
-      //       //   context,
-      //       //   MaterialPageRoute(
-      //       //     builder: (context) => User(),
-      //       //   ),
-      //       // );
-      //     },
-      //     icon: Icon(Icons.person),
-      //   ),
-      // ]),
+      backgroundColor: Color.fromARGB(146, 246, 246, 246),
       body: SafeArea(
         child: Column(
           children: [
             SizedBox(height: 16),
-            ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(20)
-              ),
-              child: Container(
-                color: Colors.cyan,
-                height: 190,
-                width: 380,
-                
-              ),
+            Stack(
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 16, left: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Hello, Yoga Pratama",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          Container(
+                            width: 45,
+                            height: 45,
+                            clipBehavior: Clip.antiAlias,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                            ),
+                            child: Image.asset(
+                              'assets/profil.jpeg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Container(
+                      width: double.infinity,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        ),
+                        image: DecorationImage(
+                            scale: 0.5,
+                            image: AssetImage("assets/mosque.jpeg"),
+                            fit: BoxFit.cover,
+                            alignment: Alignment.bottomRight),
+                        color: Colors.indigo,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            // SizedBox(height: 8),
-            Expanded(child: 
-            Padding(
+            Expanded(
+                child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: ListView.builder(
                 itemBuilder: (context, index) {
@@ -77,68 +111,20 @@ class _HomeState extends State<Home> {
                         ),
                       );
                     },
-                    child: Container(
-                      height: 70,
-                      // width: 300,
-                      // color: Colors.grey[200],
-            
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Row(
-                          children: [
-                            Text(listdoa[index].id.toString(), 
-                            ),
-                            SizedBox(
-                              width: 24,
-                            ),
-                            Text(listdoa[index].doa.toString()
-                            ),
-                          ],
-                        ),
+                    child: Card(
+                      color: Color(0xFFFFFFFF),
+                      child: ListTile(
+                        title: Text(listdoa[index].doa.toString()),
                       ),
                     ),
                   );
                 },
                 itemCount: listdoa.length,
               ),
-            )
-            )
+            ))
           ],
         ),
-        // child: ListView.separated(
-        //     itemBuilder: (context, index) {
-        //       return InkWell(
-        //         onTap: () {
-        //           Navigator.push(
-        //             context,
-        //             MaterialPageRoute(
-        //               builder: (context) => Detail(item: listdoa[index]),
-        //             ),
-        //           );
-        //         },
-        //         child: Container(
-        //           child: Padding(
-        //             padding: const EdgeInsets.all(16.0),
-        //             child: Row(
-        //               children: [
-        //                 Text(listdoa[index].id.toString()),
-        //                 SizedBox(
-        //                   width: 10,
-        //                 ),
-        //                 Text(listdoa[index].doa.toString()),
-        //               ],
-        //             ),
-        //           ),
-        //         ),
-        //       );
-        //     },
-        //     separatorBuilder: (context, index) {
-        //       return Divider();
-        //     },
-        //     itemCount: listdoa.length),
-            
       ),
-      
     );
   }
 }
