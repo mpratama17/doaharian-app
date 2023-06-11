06ba06e5412f12f6259cc3becba12e55ced6258b
mpratama17 <yogmhmmd17@gmail.com>
2023-06-11 14:37:41 +0700

    setup firebase messaging

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,36 @@ import 'package:tes/view/home.dart';
 import 'package:tes/view/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-Future main() async {
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(MyApp());
+// }
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  print('User granted permission: ${settings.authorizationStatus}');
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
