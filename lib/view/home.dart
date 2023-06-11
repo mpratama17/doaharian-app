62633fa2cb6a0ce0ee39b31159eb85d630ed2310
mpratama17 <yogmhmmd17@gmail.com>
2023-06-11 15:09:34 +0700

    no change

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -35,8 +35,8 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(title: Text('Doa Harian', 
-      // style: TextStyle(fontWeight: FontWeight.bold),), 
+      // appBar: AppBar(title: Text('Doa Harian',
+      // style: TextStyle(fontWeight: FontWeight.bold),),
       // elevation: 0,
       // actions: [
       //   IconButton(
@@ -52,137 +52,69 @@ class _HomeState extends State<Home> {
       //   ),
       // ]),
       body: SafeArea(
-<<<<<<< HEAD
-        child: ListView.custom(childrenDelegate: SliverChildBuilderDelegate(
-          (context, index) {
-            return InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Detail(item: listdoa[index]),
-                  ),
-                );
-              },
-              child: Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Text(listdoa[index].id.toString()),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(listdoa[index].doa.toString()),
-                    ],
-                  ),
+          child: ListView.custom(
+              childrenDelegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Detail(item: listdoa[index]),
+                ),
+              );
+            },
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text(listdoa[index].id.toString()),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(listdoa[index].doa.toString()),
+                  ],
                 ),
               ),
-              
-            );
-          },
-          childCount: listdoa.length,
-        ))
-        // ListView.separated(
-=======
-        child: Column(
-          children: [
-            SizedBox(height: 16),
-            ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(20)
-              ),
-              child: Container(
-                color: Colors.cyan,
-                height: 190,
-                width: 380,
-                
-              ),
             ),
-            // SizedBox(height: 8),
-            Expanded(child: 
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Detail(item: listdoa[index]),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      height: 70,
-                      // width: 300,
-                      // color: Colors.grey[200],
-            
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Row(
-                          children: [
-                            Text(listdoa[index].id.toString(), 
-                            ),
-                            SizedBox(
-                              width: 24,
-                            ),
-                            Text(listdoa[index].doa.toString()
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
-                itemCount: listdoa.length,
-              ),
-            )
-            )
-          ],
-        ),
-        // child: ListView.separated(
->>>>>>> main
-        //     itemBuilder: (context, index) {
-        //       return InkWell(
-        //         onTap: () {
-        //           Navigator.push(
-        //             context,
-        //             MaterialPageRoute(
-        //               builder: (context) => Detail(item: listdoa[index]),
-        //             ),
-        //           );
-        //         },
-        //         child: Container(
-        //           child: Padding(
-<<<<<<< HEAD
-        //             padding: const EdgeInsets.all(8.0),
-=======
-        //             padding: const EdgeInsets.all(16.0),
->>>>>>> main
-        //             child: Row(
-        //               children: [
-        //                 Text(listdoa[index].id.toString()),
-        //                 SizedBox(
-        //                   width: 10,
-        //                 ),
-        //                 Text(listdoa[index].doa.toString()),
-        //               ],
-        //             ),
-        //           ),
-        //         ),
-        //       );
-        //     },
-        //     separatorBuilder: (context, index) {
-        //       return Divider();
-        //     },
-        //     itemCount: listdoa.length),
-<<<<<<< HEAD
-=======
-            
->>>>>>> main
-      ),
-      
+          );
+        },
+        childCount: listdoa.length,
+      ))
+          // ListView.separated(
+          //     itemBuilder: (context, index) {
+          //       return InkWell(
+          //         onTap: () {
+          //           Navigator.push(
+          //             context,
+          //             MaterialPageRoute(
+          //               builder: (context) => Detail(item: listdoa[index]),
+          //             ),
+          //           );
+          //         },
+          //         child: Container(
+          //           child: Padding(
+          //             padding: const EdgeInsets.all(16.0),
+          //             child: Row(
+          //               children: [
+          //                 Text(listdoa[index].id.toString()),
+          //                 SizedBox(
+          //                   width: 10,
+          //                 ),
+          //                 Text(listdoa[index].doa.toString()),
+          //               ],
+          //             ),
+          //           ),
+          //         ),
+          //       );
+          //     },
+          //     separatorBuilder: (context, index) {
+          //       return Divider();
+          //     },
+          //     itemCount: listdoa.length),
+
+          ),
     );
   }
 }
